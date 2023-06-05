9d11ed3f3b5ddefda9d7a14d503bf19788673167
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-06-05 17:10:55 -0300

    Update index.bat

--- index.bat
+++ index.bat
@@ -1,36 +1,35 @@
-echo ---CONFIGs INICIAIS--- 
-echo off
-chcp 65001
-title Central De Atalhos
-goto INICIO 
-  
----------------------------------------------
+@echo off
+echo ___CONFIGs INICIAIS___
+if "%1" neq "" goto %1
+chcp 65001
+title Central De Atalhos
+setlocal
 
-:INICIO 
-mode con:cols=50 lines=30 
-color 02 
-cls 
-echo. 
-echo         --- GUIA DE ATALHOS --- 
-echo. 
-echo. 
-echo. 
-echo.         Digite um dos valores abaixo :  
-echo. 
-echo.        1 - SITES ( PESSOAIS )
-echo.        2 - SITES ( ESTÁGIOS / JOBS )
-echo.        3 - SITES ( CURSOS )
-echo.        4 - CANAIS YOUTUBE
-echo. 
-set /p opc=.        : 
-cls 
-  
-if %opc% == 1 goto cmd start pessoais.bat
-if %opc% == 2 goto cmd start jobs.bat
-if %opc% == 3 goto cmd start cursos.bat
-if %opc% == 4 goto cmd start youtube.bat
+:INICIO
+mode con:cols=50 lines=30
+color 02
+cls
+echo.
+echo          ___ GUIA DE ATALHOS ___
+echo.
+echo.
+echo.
+echo    Digite um dos valores abaixo:
+echo.
+echo    (1) SITES PESSOAIS
+echo    (2) SITES ESTAGIOS E JOBS
+echo    (3) SITES CURSOS
+echo    (4) CANAIS YOUTUBE
+echo.
+set /p opc=Insira um valor:
+cls
 
-if %opc% gtr 15 goto cmd start errornum.bat
-if %opc% lss 1 goto cmd start errornum.bat
+if %opc% equ 1 goto start pessoais.bat
+if %opc% equ 2 goto start jobs.bat
+if %opc% equ 3 goto start cursos.bat
+if %opc% equ 4 goto start youtube.bat
 
---------------------------------------------- 
+if %opc% gtr 4 goto start errornum.bat
+if %opc% lss 1 goto start errornum.bat
+
+--------------------------------------------
