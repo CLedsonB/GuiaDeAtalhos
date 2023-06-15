981634e13aa16960911da91f3ade56a8555e5832
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-06-14 21:47:20 -0300

    Update cursos.bat

--- cursos.bat
+++ cursos.bat
@@ -20,21 +20,23 @@ __________________________________________
 
 :INICIO 
 mode con:cols=50 lines=30 
-color 02 
+color 0d
 cls
 echo. 
 echo          --- SITES ( CURSOS ) --- 
 echo. 
 echo. 
 echo. 
-echo.          Digite um dos valores abaixo :
+echo.        Escolha um dos valores abaixo :
 echo.
 echo.        (1) COURSERA
 echo.        (2) EBAC
 echo.        (3) FREE CODE CAMP
 echo.        (4) FRONTEND MENTOR
+echo.
+echo.        (0) Voltar ao Guia de Atalhos
 echo. 
-set /p opc=.	Insira um valor : 
+set /p opc=. --- Digite e presssione [ENTER] : 
 
 mode con:cols=50 lines=15 
 cls
@@ -43,9 +45,10 @@ if %opc% equ 1 goto COURSERA
 if %opc% equ 2 goto EBAC
 if %opc% equ 3 goto FCC
 if %opc% equ 4 goto FEM
+if %opc% equ 0 start index.bat && exit
 
 if %opc% gtr 4 start errornum.bat && timeout 3 && start cursos.bat
-if %opc% lss 1 start errornum.bat && timeout 3 && start cursos.bat
+if %opc% lss 0 start errornum.bat && timeout 3 && start cursos.bat
 
 __________________________________________
 
@@ -120,15 +123,15 @@ mode con:cols=50 lines=10
 cls 
 echo. 
 echo. 
-echo.        Escolha uma das opções a seguir :
+echo.   Escolha uma das opções a seguir :
 echo.
 echo.     (1) Voltar ao Guia de Atalhos
-echo.     (2) Voltar ao Atalhos de curso
+echo.     (2) Voltar ao Atalhos YouTube 
 echo.     (3) Encerrar seção
+echo.
+choice /c 123 /n /m "--- Digite aqui : " 
+cls
 
-set /p opc=.   Insira um valor : 
-cls 
-  
-if %opc% equ 1 start index.bat && exit
-if %opc% equ 2 goto INICIO
-if %opc% equ 3 exit
+if errorlevel 3 exit
+if errorlevel 2 goto INICIO
+if errorlevel 1 start index.bat && exit
