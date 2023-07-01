679e9f16547103cd6903fe5c443382f61d5127b8
CLedsonB <74743220+CLedsonB@users.noreply.github.com>
2023-06-30 22:38:44 -0300

    Update index.bat

--- index.bat
+++ index.bat
@@ -3,17 +3,17 @@ echo --- CONFIGs INICIAIS ---
 chcp 65001
 title Central De Atalhos
 setlocal
-
-set "Ecld=cledsonborgesdossantos@gmail.com"
-set "Eedz=edzzon2@gmail.com"
-set "Eifba=20190028012@ifba.edu.br"
-set "Sn=34012348"
-set "Sns=34012348@!"
-set "Snsl=34012348@!a"
-set "Snls=34012348a@!"
-set "Sifba=ifba.85932145501"
-set "Sm=Ma@v6lne$%"
-set "SM=MA@V6LNE$%"
+set "Ecld=emailpessoal@gmail.com"
+set "Eedz=bacdefghi@gmail.com"
+set "Eifba=929580203905@ifba.edu.br"
+set "Sn=934-53"
+set "Sns=80753lks"
+set "Snsl=jdhsg234"
+set "Snls=25971absc"
+set "SnlsS=69795kfjd"
+set "Sifba=SENHAnvalor"
+set "Sm=senhaABC%"
+set "SM=senha1234"
 
 goto INICIO
 
@@ -24,7 +24,8 @@ mode con cols=50 lines=30
 color 0a
 cls
 echo.
-echo          --- GUIA DE ATALHOS ---
+echo.
+echo         --- GUIA DE ATALHOS ---
 echo.
 echo.
 echo.
@@ -38,8 +39,8 @@ echo.
 choice /c 12345 /n /m "--- Digite aqui : " 
 cls
 
-if errorlevel 5 call bug109.bat && exit
-if errorlevel 4	call youtube.bat && exit
-if errorlevel 3	call cursos.bat %Ecld% %Eedz% %Eifba% %Sn% %Sns% %Snsl% %Snls% %Sifba% %Sm% %SM%
-if errorlevel 2 call jobs.bat %Ecld% %Eedz% %Eifba% %Sn% %Sns% %Snsl% %Snls% %Sifba% %Sm% %SM%
-if errorlevel 1 call pessoais.bat %Ecld% %Eedz% %Eifba% %Sn% %Sns% %Snsl% %Snls% %Sifba% %Sm% %SM%
+if errorlevel 5 call error/bug109.bat && exit
+if errorlevel 4	call main/youtube.bat && exit
+if errorlevel 3	call main/cursos.bat %Ecld% %Eedz% %Eifba% %Sn% %Sns% %Snsl% %Snls% %Sifba% %Sm% %SM%
+if errorlevel 2 call main/jobs.bat %Ecld% %Eedz% %Eifba% %Sn% %Sns% %Snsl% %Snls% %Sifba% %Sm% %SM%
+if errorlevel 1 call main/pessoais.bat %Ecld% %Eedz% %Eifba% %Sn% %Sns% %Snsl% %Snls% %Sifba% %Sm% %SM% %SnlsS%
